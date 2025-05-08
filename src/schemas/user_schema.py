b89e8995cd42d61e9c87857ec1e0dad8cd81b585
Dhruv Patel <dhruv.n.patel@simformsolutions.com>
2025-05-08 15:37:17 +0530

    Add password validation and confirm password check in user schema; enhance password strength requirements

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -1,4 +1,5 @@
-from pydantic import UUID4, BaseModel, EmailStr, Field, field_validator
+from pydantic import UUID4, BaseModel, EmailStr, Field, field_validator  , model_validator
+from ..utils.password_helper import validate_password
 
 
 class User(BaseModel):
@@ -9,7 +10,24 @@ class UserInput(User):
     username: str
     email: EmailStr
     password: str
+    confirm_password: str
 
+    @field_validator('username')
+    def validate_username_field(cls,value):
+        if " " in value:
+            raise ValueError("Username cannot contain spaces")
+    
+    @field_validator('password')
+    def validate_password_field(cls,value):
+        if validate_password(password= value):
+            return value
+        raise ValueError("Password must be strong")
+    
+    @model_validator(mode="after")
+    def validate_password_and_confirm_password(self):
+        if self.password != self.confirm_password:
+            raise ValueError("password and confirm password is not same")
+        return self
 
 class UserInDb(User):
     id: UUID4
--- src/utils/password_helper.py
+++ src/utils/password_helper.py
@@ -1,6 +1,9 @@
+import re
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
+REG = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+PATTERN  = re.compile(REG)
 
 class PasswordIn(BaseModel):
     password: str
@@ -19,3 +22,6 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hash_password: str) -> bool:
     return pwd_context.verify(plain_password, hash_password)
+
+def validate_password(password: str) -> bool:
+    return re.search(PATTERN, password)
